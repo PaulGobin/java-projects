4bfbf507c6d2383e2567032bfe1c11be16ee9158
PaulGobin <PaulGobin@Gmail.com>
2017-10-03 19:39:40 -0400

    ssss

---
+++ test-git-project/src/main/java/com/bvn/test/TestGitProjectApplication.java
@@ -0,0 +1,12 @@
+package com.bvn.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitProjectApplication.class, args);
+	}
+}
---
+++ test-git-project/src/test/java/com/bvn/test/TestGitProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bvn.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestGitProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
