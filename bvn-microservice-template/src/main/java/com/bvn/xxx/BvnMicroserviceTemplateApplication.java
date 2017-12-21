036eba76d1d0e0201f27ed979ed8bfeaf864a0ed
PaulGobin <Paul.Gobin@windstream.com>
2017-12-20 22:06:37 -0500

    initial commit

---
+++ bvn-microservice-template/src/main/java/com/bvn/xxx/BvnMicroserviceTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.bvn.xxx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BvnMicroserviceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BvnMicroserviceTemplateApplication.class, args);
+	}
+}
---
+++ bvn-microservice-template/src/test/java/com/bvn/xxx/BvnMicroserviceTemplateApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bvn.xxx;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BvnMicroserviceTemplateApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
